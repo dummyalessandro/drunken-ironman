7ec5d70b1f394da3c9ef8349ce2fcb75882b8637
Tecnosphera SpA <Tecnosphera SpA@192.168.1.137>
2015-05-26 11:56:00 +0200

    secondo commit

--- progettoTest/src/main/java/progettoTest/HelloWorld.java
+++ progettoTest/src/main/java/progettoTest/HelloWorld.java
@@ -21,5 +21,52 @@ public class HelloWorld
 		return "HelloWorld [message=" + message + "]";
 	}
 
+	public HelloWorld(final String message)
+	{
+		super();
+		this.message = message;
+	}
+
+	@Override
+	public int hashCode()
+	{
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((message == null) ? 0 : message.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(final Object obj)
+	{
+		if (this == obj)
+		{
+			return true;
+		}
+		if (obj == null)
+		{
+			return false;
+		}
+		if (getClass() != obj.getClass())
+		{
+			return false;
+		}
+		final HelloWorld other = (HelloWorld) obj;
+		if (message == null)
+		{
+			if (other.message != null)
+			{
+				return false;
+			}
+		}
+		else if (!message.equals(other.message))
+		{
+			return false;
+		}
+		return true;
+	}
+
+
+
 
 }
