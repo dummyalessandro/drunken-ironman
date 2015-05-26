82caaa6248a50cc64e1993797d69c84b5289daa4
Tecnosphera SpA <Tecnosphera SpA@192.168.1.137>
2015-05-26 10:57:41 +0200

    primo import

--- progettoTest/src/main/java/progettoTest/HelloWorld.java
+++ progettoTest/src/main/java/progettoTest/HelloWorld.java
@@ -1,14 +1,25 @@
 package progettoTest;
 
 
-public class HelloWorld {
-   private String message;
+public class HelloWorld
+{
+	private String message;
 
-   public void setMessage(String message){
-      this.message  = message;
-   }
+	public void setMessage(final String message)
+	{
+		this.message = message;
+	}
 
-   public void getMessage(){
-      System.out.println("Your Message : " + message);
-   }
+	public void getMessage()
+	{
+		System.out.println("Your Message : " + message);
+	}
+
+	@Override
+	public String toString()
+	{
+		return "HelloWorld [message=" + message + "]";
+	}
+
+
 }
